824c6528107bfdfd7ffc9a118debb2c31fe6bd85
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-08-31 15:51:33 +0200

    add ET+ and WC methods in command

--- src/Tickets_Plus/Command.php
+++ src/Tickets_Plus/Command.php
@@ -1,4 +1,94 @@
 <?php
+
 class Tribe__CLI__Tickets_Plus__Command extends WP_CLI_Command {
 
+	/**
+	 * @var \Tribe__CLI__Tickets__Plus_Generator__WooCommerce__CLI
+	 */
+	protected $wc_generator;
+
+	/**
+	 * Tribe__CLI__Tickets_Plus__Command constructor.
+	 *
+	 * @param \Tribe__CLI__Tickets__Plus_Generator__WooCommerce__CLI $wc_generator
+	 */
+	public function __construct( Tribe__CLI__Tickets__Plus_Generator__WooCommerce__CLI $wc_generator ) {
+		parent::__construct();
+		$this->wc_generator = $wc_generator;
+	}
+
+	/**
+	 * Generates WooCommerce orders for a WooCommerce ticketed post.
+	 *
+	 * ## OPTIONS
+	 *
+	 * <post_id>
+	 * : orders ill be attached to this post
+	 *
+	 * [--count=<count>]
+	 * : the number of orders to generate
+	 * ---
+	 * default: 10
+	 * ---
+	 *
+	 * [--tickets_min=<tickets_min>]
+	 * : the minimum number of tickets per order
+	 * ---
+	 * default: 1
+	 * ---
+	 *
+	 * [--tickets_max=<tickets_max>]
+	 * : the maximum number of tickets per order
+	 * ---
+	 * default: 3
+	 * ---
+	 *
+	 * [--ticket_status=<ticket_status>]
+	 * : the order status of the tickets
+	 * ---
+	 * default: random
+	 * options:
+	 *      - yes
+	 *      - no
+	 * ---
+	 *
+	 * [--ticket_id=<ticket_id>]
+	 * : the ID of the ticket orders should be assigned to
+	 *
+	 * ## EXAMPLES
+	 *
+	 *      wp event-tickets-plus generate-orders 23
+	 *      wp event-tickets-plus generate-orders 23 --count=89
+	 *      wp event-tickets-plus generate-orders 23 --tickets_min=3
+	 *      wp event-tickets-plus generate-orders 23 --tickets_min=3 --tickets_max=10
+	 *      wp event-tickets-plus generate-orders 23 --tickets_min=3 --tickets_max=10 --ticket_status=no
+	 *      wp event-tickets-plus generate-orders 23 --ticket_id=89
+	 *
+	 * @subcommand generate-wc-orders
+	 */
+	public function generate_wc_orders( array $args = null, array $assoc_args = null ) {
+		$this->wc_generator->generate_orders( $args, $assoc_args );
+	}
+
+	/**
+	 * Removes all WC orders from a WooCommerce ticketed post.
+	 *
+	 * ## OPTIONS
+	 *
+	 * <post_id>
+	 * : orders will be removed from this ticketed post
+	 *
+	 * [--ticket_id=<ticket_id>]
+	 * : only remove orders for this WooCommerce ticket attached to the post
+	 *
+	 * ## EXAMPLES
+	 *
+	 *      wp event-tickets-plus reset-wc-orders 23
+	 *      wp event-tickets-plus reset-wc-orders 23 --ticket_id=89
+	 *
+	 * @subcommand reset-wc-orders
+	 */
+	public function reset_wc_orders( array $args = null, array $assoc_args = null ) {
+		$this->wc_generator->reset_orders( $args, $assoc_args );
+	}
 }
