32ea191f9150583a7b362615ccdebf8a29891b3b
Matthew Batchelder <borkweb@gmail.com>
2019-03-08 08:39:36 -0500

    Removing more die

--- src/Documentation/Abstract_Doc_Command.php
+++ src/Documentation/Abstract_Doc_Command.php
@@ -19,17 +19,14 @@ abstract class Abstract_Doc_Command extends \WP_CLI_Command {
 		$plugin = reset( $args );
 		if ( ! $plugin ) {
 			WP_CLI::error( 'You must specify a plugin' );
-			die;
 		}
 
 		if ( $plugin !== preg_replace( '![^a-zA-Z0-9\-\_]!', '', $plugin ) ) {
 			WP_CLI::error( 'You have supplied an invalid plugin name' );
-			die;
 		}
 
 		if ( ! file_exists( TRIBE_CLI_DIR . '/../' . $plugin ) ) {
 			WP_CLI::error( 'You must specify a plugin directory that actually exists' );
-			die;
 		}
 
 		$this->plugin_dir = TRIBE_CLI_DIR . '/../' . $plugin;
