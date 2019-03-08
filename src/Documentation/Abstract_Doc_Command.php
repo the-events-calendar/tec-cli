a01600c807003566e5d7f57ff992915582f54eae
Matthew Batchelder <borkweb@gmail.com>
2019-03-08 08:42:23 -0500

    Adding more __() calls

--- src/Documentation/Abstract_Doc_Command.php
+++ src/Documentation/Abstract_Doc_Command.php
@@ -18,15 +18,15 @@ abstract class Abstract_Doc_Command extends \WP_CLI_Command {
 	protected function parse_plugin( array $args ) {
 		$plugin = reset( $args );
 		if ( ! $plugin ) {
-			WP_CLI::error( 'You must specify a plugin' );
+			WP_CLI::error( __( 'You must specify a plugin', 'tribe-cli' ) );
 		}
 
 		if ( $plugin !== preg_replace( '![^a-zA-Z0-9\-\_]!', '', $plugin ) ) {
-			WP_CLI::error( 'You have supplied an invalid plugin name' );
+			WP_CLI::error( __( 'You have supplied an invalid plugin name', 'tribe-cli' ) );
 		}
 
 		if ( ! file_exists( TRIBE_CLI_DIR . '/../' . $plugin ) ) {
-			WP_CLI::error( 'You must specify a plugin directory that actually exists' );
+			WP_CLI::error( __( 'You must specify a plugin directory that actually exists', 'tribe-cli' ) );
 		}
 
 		$this->plugin_dir = TRIBE_CLI_DIR . '/../' . $plugin;
